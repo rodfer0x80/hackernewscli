d76e2d39c0a9dcd5e0cd68f2e6e9365be04778b7
rodfer0x80 <rodrigolf080@gmail.com>
2022-09-02 14:20:39 +0100

    template

---
+++ src/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from hackernewscli.bootstrap import runme
+
+def main():
+    runme()
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/hackernewscli/bootstrap.py
@@ -0,0 +1,16 @@
+from . import *
+#from example import example
+
+import sys
+
+## Configs
+
+
+
+##
+
+def runme():
+    print("boot")
+    example()
+    return 0
+
